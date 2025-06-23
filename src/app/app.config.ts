639737c1d37b524c37ebd001df6c6a377eb0191c
Jacob Benowitz <jacob.benowitz@gmail.com>
2025-06-23 12:03:24 -0400

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "angular-proj-2b3b6", appId: "1:662218297498:web:254fd723f8bc38214b4f23", storageBucket: "angular-proj-2b3b6.firebasestorage.app", apiKey: "AIzaSyDBkheHtdu9oI_Atc5I9hpTidgdBqXgB54", authDomain: "angular-proj-2b3b6.firebaseapp.com", messagingSenderId: "662218297498", measurementId: "G-Z7TWLG54QB" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
